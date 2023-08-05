580fc96e817c4cf244420aa6cbb500f1f2bb00f3
ryv10 <74744128+ryvir@users.noreply.github.com>
2023-08-05 14:55:57 -0700

    Add about page

---
+++ src/routes/about/+page.server.js
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit'
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params, locals }) {
+	console.log('about server load')
+}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>About - PleasantVue</title>
+</svelte:head>
+
+<div class="layout" data-sveltekit-prefetch>
+    <div class="grid-heading-container">
+		<h1>About</h1>
+	</div>
+    <p>PleasantVue was created and is maintained by Amador Valley seniors Ryan Vir and Aayuman Ghangurde. We felt that the student portal experience would be elevated with the addition of more features that would create a super-app for PUSD. Certain features, such as the calendar and staff list, were developed up to one year ago. It was all put together with the StudentVUE integration after PUSD switched its SIS away from Q.<br><br></p>
+
+    <h2>Privacy</h2>
+    <p></p>
+
+    <h2>Features</h2>
+    <p>PleasantVue has many exclusive features. These include:</p>
+    <ul>
+        <li>Calendar and Bell Schedules</li>
+        <li>Staff Directory</li>
+        <li>Notifications</li>
+        <li>Gradebook Graphs</li>
+        <li>Gradebook Stats</li>
+        <li>Simulated Assignments</li>
+        <li>Schedule Sharing</li>
+        <li>School Map</li>
+    </ul>
+    <p>Please contact us if you have any feature ideas.<br><br></p>
+    
+    <h2>License</h2>
+    <p>PleasantVue is now an open source project licensed under GPL-3 and you can view the source code at <a href="https://github.com/PleasantVue/PleasantVue">github.com/PleasantVue/PleasantVue</a>.<br><br></p>
+
+    <h2>Contact</h2>
+    <p>If you have any questions or feedback, please contact us at <a href="mailto:pleasantvue@gmail.com">pleasantvue@gmail.com</a>. We'd love to hear from you! We are also building out features for more schools, so if you attend a school that doesn't have some features, please reach out to us help out.</p>
+</div>
