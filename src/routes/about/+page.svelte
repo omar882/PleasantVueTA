379ba3522aece0681395fcbb375016fe058998c0
thiccaxe <51303986+thiccaxe@users.noreply.github.com>
2023-08-05 17:49:26 -0700

    privacy info

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,7 +13,9 @@
     <p>PleasantVue was created and is maintained by Amador Valley seniors Ryan Vir and Aayuman Ghangurde. We felt that the student portal experience would be elevated with the addition of more features that would create a super-app for PUSD. Certain features, such as the calendar and staff list, were developed up to one year ago. It was all put together with the StudentVUE integration after PUSD switched its SIS away from Q.<br><br></p>
 
     <h2>Privacy</h2>
-    <p></p>
+    <p>Our server does not log anything and stores no information about you or your data. You can read every line single of code we run for yourself on our github, linked below</p>
+    <p>The exception to this is the "Share Schedule" feature. We will anonymously store your schedule, including room, teacher and period data.</p>
+    <p>Want your data deleted? send us an email using your school gmail account (as proof of identity) and your student id to our contact.</p>
 
     <h2>Features</h2>
     <p>PleasantVue has many exclusive features. These include:</p>
@@ -31,6 +33,8 @@
     
     <h2>License</h2>
     <p>PleasantVue is now an open source project licensed under GPL-3 and you can view the source code at <a href="https://github.com/PleasantVue/PleasantVue">github.com/PleasantVue/PleasantVue</a>.<br><br></p>
+    <p>PleasantVue is a fork of Gradenight, created by refact0r: <a href="https://github.com/refact0r/gradenight/">https://github.com/refact0r/gradenight/</a></p>
+    <p>Thanks to the StudentVue organization on github for docs and the studentvue.js library</p>
 
     <h2>Contact</h2>
     <p>If you have any questions or feedback, please contact us at <a href="mailto:pleasantvue@gmail.com">pleasantvue@gmail.com</a>. We'd love to hear from you! We are also building out features for more schools, so if you attend a school that doesn't have some features, please reach out to us help out.</p>
